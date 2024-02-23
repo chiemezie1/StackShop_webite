7f69cad37bd381d77bd0605fe6c86192a6d167d1
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-23 18:35:10 +0100

    moved from react to next

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import { useRouter } from 'next/router';
-
-// const Modal = ({ closeModal }) => {
-//   const router = useRouter();
-
-//   const goToCart = () => {
-//     closeModal();
-//     router.push('/cart');
-//   };
-
-//   return (
-//     <div className="ModalOverlay">
-//       <div className="Modal">
-//         <p>Item successfully added to cart!</p>
-//         <button onClick={goToCart}>Go to Cart</button>
-//         <button onClick={closeModal}>Continue Shopping</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Modal;
-
-
 import React from 'react';
 import { useRouter } from 'next/router';
 
